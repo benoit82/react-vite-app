2f65255d41a01a052f691ffe26853045720b7acf
Benoit <benoit.dubus@outlook.fr>
2022-11-06 01:26:02 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <a href="http://www.google.fr/" target='_blank'>Google, pour vos recherches...</a>
+    </>
+  )
+}
+
+export default App
