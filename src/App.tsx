0ca23109521fb6d877170eb8b8ead2ff3b4f5ac5
Benoit <benoit.dubus@outlook.fr>
2022-11-11 12:28:03 +0100

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import { FC } from 'react';
-import { CustomTheme, ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
-import { Container, NestedContainer } from './components/Container';
-import { useStore } from './store/mainStore';
+import { ThemeProvider } from 'styled-components';
+import { Button } from '@components/Button';
+import { Container, NestedContainer } from '@components/Container';
+import { useStore } from '@store/mainStore';
+import { darkTheme, lightTheme } from '@themes/index';
 
 const App: FC = () => {
   const { darkThemeStatus, count, toggleTheme } = useStore((state) => state);
 
-  const darkTheme: CustomTheme = {
-    primary: '#242424',
-    secondary: '#d5d5d5',
-  };
-
-  const theme: CustomTheme = {
-    primary: '#dfdfdf',
-    secondary: '#060d07',
-  };
-
   return (
-    <ThemeProvider theme={darkThemeStatus ? darkTheme : theme}>
+    <ThemeProvider theme={darkThemeStatus ? darkTheme : lightTheme}>
       <Container textColor="green">
         <p>
           count : <span>{count}</span>
