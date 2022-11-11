2f715e44cda3186fe5030d6a0435d9886019b691
Benoit Dubus <benoit.dubus@outlook.fr>
2022-11-11 12:29:10 +0100

    refactoring code (#2)

---
+++ src/themes/darkTheme.ts
@@ -0,0 +1,8 @@
+import { CustomTheme } from 'styled-components';
+
+const darkTheme: CustomTheme = {
+  primary: '#242424',
+  secondary: '#d5d5d5',
+};
+
+export default darkTheme;
---
+++ src/themes/lightTheme.ts
@@ -0,0 +1,8 @@
+import { CustomTheme } from 'styled-components';
+
+const lightTheme: CustomTheme = {
+  primary: '#dfdfdf',
+  secondary: '#060d07',
+};
+
+export default lightTheme;
