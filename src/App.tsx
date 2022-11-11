d27da7bf2d26c577ece104d8176648b74e39bfeb
Benoit <benoit.dubus@outlook.fr>
2022-11-11 10:54:29 +0100

    update prettier rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react'
-import { CustomTheme, ThemeProvider } from 'styled-components'
+import { FC } from 'react';
+import { CustomTheme, ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
-import { Container, NestedContainer } from './components/Container'
+import { Container, NestedContainer } from './components/Container';
 import { useStore } from './store/mainStore';
 
 const App: FC = () => {
@@ -10,23 +10,25 @@ const App: FC = () => {
   const darkTheme: CustomTheme = {
     primary: '#242424',
     secondary: '#d5d5d5',
-  }
+  };
 
   const theme: CustomTheme = {
     primary: '#dfdfdf',
     secondary: '#060d07',
-  }
+  };
 
   return (
-    <ThemeProvider theme={darkThemeStatus ? darkTheme : theme }>
-      <Container textColor='green' >
-        <p>count : <span>{count}</span></p>
+    <ThemeProvider theme={darkThemeStatus ? darkTheme : theme}>
+      <Container textColor="green">
+        <p>
+          count : <span>{count}</span>
+        </p>
         <NestedContainer>
           <Button onClick={() => toggleTheme()}>switch theme</Button>
         </NestedContainer>
       </Container>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/store/mainStore.ts
+++ src/store/mainStore.ts
@@ -1,13 +1,17 @@
-import create from "zustand";
+import create from 'zustand';
 
 interface MainStore {
   darkThemeStatus: boolean;
   count: number;
-  toggleTheme: (state: MainStore) => void;
+  toggleTheme: () => void;
 }
 
 export const useStore = create<MainStore>((set) => ({
   darkThemeStatus: true,
   count: 0,
-  toggleTheme: () => (set((state) => ({ darkThemeStatus: !state.darkThemeStatus, count: state.count +1 }))),
+  toggleTheme: () =>
+    set((state) => ({
+      darkThemeStatus: !state.darkThemeStatus,
+      count: state.count + 1,
+    })),
 }));
