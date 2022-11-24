eab49fb59458cc309364203a402b849dbfbdada0
Benoit <benoit.dubus@outlook.fr>
2022-11-24 01:26:51 +0100

    update test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from 'App';
 
 describe('<App />', () => {
   it('should display correctly', () => {
-    const { container } = render(<App />);
-    expect(container).toBeInTheDocument();
+    render(<App />);
+    const text = screen.getByText(/switch theme counter :/i);
+
+    expect(text).toBeInTheDocument();
   });
 });
